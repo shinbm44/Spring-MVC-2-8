4f897ba03e1e6da67b697ce0a6f1b68bba11638a
hyunjin_byun <shinbm219@naver.com>
2024-02-13 19:28:39 +0900

    [섹션11]파일업로드:파일 저장 기능

---
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,52 @@
+package hello.upload.controller;
+
+import hello.upload.domain.Item;
+import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form){
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        //데이터 베이스에 저장
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+
+    }
+
+
+}
